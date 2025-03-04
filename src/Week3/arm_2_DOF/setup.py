907eb1c867907f6f9908f7dbc4b4d60851529814
richy-gs <jesusrg2405@gmail.com>
2025-03-03 23:29:12 -0600

    Practice #2 Arm with 2 DOF by Phd. Armando Miranda

---
+++ src/Week3/arm_2_DOF/setup.py
@@ -0,0 +1,37 @@
+import os
+from glob import glob
+
+from setuptools import find_packages, setup
+
+package_name = "arm_2_DOF"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
+        (
+            os.path.join("share", package_name, "config"),
+            glob(os.path.join("config", "*.[yma]*")),
+        ),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="roli_005",
+    maintainer_email="jesusrg2405@gmail.com",
+    description="TODO: Package description",
+    license="Apache-2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "set_point = arm_2_DOF.set_point:main",
+            "controller = arm_2_DOF.controller:main",
+        ],
+    },
+)
